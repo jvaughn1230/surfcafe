ada1d07d59826005664fdb1b5322613064895f2d
Jeffrey Vaughn <jvaughn1230@gmail.com>
2022-12-19 12:41:47 -0800

    adding resources & outline

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './hero.styles.css'
+
+const Hero = () => {
+  return (
+    <div className='heroSection'>
+      Hero Section
+    </div>
+  )
+}
+
+export default Hero
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import Hero from "../components/hero/Hero";
+import About from "../components/About";
+import Chef from "../components/Chef";
+import Contact from "../components/Contact";
 
 const Homepage = () => {
   return (
     <main>
-        Homepage
+      <Hero />
+      <About />
+      <Chef />
+      <Contact />
     </main>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
